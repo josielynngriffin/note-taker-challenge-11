d153a19c23044f7b56c27968e4b978741a6d0523
josielynngriffin <jelynngriffin@gmail.com>
2023-05-31 15:52:11 -0400

    Add read functions and get route for api/notes

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const fs = require('fs');
+const util=require('util');
+const uuid= require('uuid');
 //set static folder
 app.use(express.static('public'));
 //body parser middleware and to handle form data
@@ -20,9 +22,30 @@ app.get('/', (req,res)=> {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 });
+//fs promises
+const readFromFile = util.promisify(fs.readFile);
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
 
-//GET /api/notes
 //read db.json and return saved notes as json
+app.get('/api/notes', (req,res) => {
+    console.log(`${req.method} request recieved`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    
+});
 
 //POST /api/notes
 //should recieve new note to save on request body, add it to db.json, then return the new note to the client
